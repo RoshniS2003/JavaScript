027f22060e5a29009972675faf8f51f055c38797
Roshni <roshnisahu11222@gmail.com>
2026-06-10 23:50:23 +0530

    updated slice and splice concepts

--- Intermediate/2_Array.js
+++ Intermediate/2_Array.js
@@ -9,24 +9,22 @@ const colours = ["red", "pink", "white"]
 // console.log(items);
 
 // best and easy
-// const items = [...flower, ...colours]
-// console.log(items);
+const items = [...flower, ...colours]
+console.log(items);
 
 // console.log("====================>>");
-// const arrNum = [1, 2, [3, 4, 5], 6, 7, [8, 7, [5, 3, 0]]]
-// const arrNum1 = arrNum.flat(Infinity)
-// console.log(arrNum1);
+const arrNum = [1, 2, [3, 4, 5], 6, 7, [8, 7, [5, 3, 0]]]
+const arrNum1 = arrNum.flat(Infinity)
+console.log(arrNum1);
 
-console.log("----------------->>>>");
+// console.log("----------------->>>>");
 console.log(Array.isArray("Roshni"));
 console.log(Array.from("Roshni"));
 console.log(Array.from({name :"Roshni"}));
 
-console.log("----------------->>>");
+// console.log("----------------->>>");
 const score1 = 100
 const score2 = 200
 const score3 = 300
 
 console.log(Array.of(score1 , score2 , score3));
-
-
