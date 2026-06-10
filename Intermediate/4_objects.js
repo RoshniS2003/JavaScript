dbf5845b8f9929c1848899aec627cb5583bbb4e5
Roshni <roshnisahu11222@gmail.com>
2026-06-11 01:45:13 +0530

    Added singleton concepts

---
+++ Intermediate/4_objects.js
@@ -0,0 +1,9 @@
+const objUser = new Object()    // singleton objects
+// const objUser = {}       // non-singleton objects
+console.log(objUser);
+
+objUser.name = "roshni"
+objUser.id = "rani@123"
+objUser.IsloggedIn = false
+
+console.log(objUser);
