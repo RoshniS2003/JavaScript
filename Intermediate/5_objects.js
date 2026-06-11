1b763779b734e5d20cab2388ba2dfeabcb0dd1bb
Roshni <roshnisahu11222@gmail.com>
2026-06-11 19:59:06 +0530

    learn object de-structure concepts

---
+++ Intermediate/5_objects.js
@@ -0,0 +1,9 @@
+// Object de-structure
+const course = {
+    courseName : "JavaScript",
+    price : "999Rs.",
+    courseInstructor : "Roshni"
+}
+// course.courseInstructor
+const {courseInstructor} = course
+console.log(courseInstructor);
