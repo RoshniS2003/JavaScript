4c49f30106c7238408d55261a63d8f48aa58da33
Roshni <roshnisahu11222@gmail.com>
2026-06-14 23:37:50 +0530

    Added switch concepts

---
+++ 03_Control_flow/3_switch.js
@@ -0,0 +1,43 @@
+// Number
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default month case");
+        break;
+}
+// Note : If case 3 comment the break then case 3 & 4 value is print
+
+
+// String
+const month1 = "feb";
+switch (month1) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default month case");
+        break;
+}
